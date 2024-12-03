17c1a4bd3246b82306722b876c02b4c821e7612c
github-cloudlabsuser-1363 <168110128+github-cloudlabsuser-1363@users.noreply.github.com>
2024-12-03 14:30:59 +0000

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -9353,7 +9353,7 @@ const axios = __nccwpck_require__(8757)
 
 const OPEN_AI_MODEL = "gpt-3.5-turbo"
 const OPENAI_API = axios.create({
-  baseURL: "https://open-ai-contoso.openai.azure.com/openai/deployments/gpt-35-turbo/chat/completions?api-version=2024-02-15-preview",
+  baseURL: "https://githubaction1530090.openai.azure.com/openai/deployments/gpt-35-turbo/chat/completions?api-version=2024-02-15-preview",
   headers: {
     "Content-Type": "application/json",
     "api-key": `${process.env.OPENAI_TOKEN}`,
